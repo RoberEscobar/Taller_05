a78b3536ded55a35ba53a57c0d2315d59f661504
Roberto Escobar <rescobar8@cuc.edu.co>
2024-09-19 11:28:29 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import proyecto from "./assets/img/descarga.png"
+function App() {
+
+
+  return (
+    <>
+ <div>
+  <img src ={proyecto} alt="Proyecto05" />
+  <h1>Lionel Andres Messi</h1>
+  <p>El mejor jugador de la historia</p>
+
+ </div>
+    </>
+  )
+}
